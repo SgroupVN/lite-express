f2701874700a343634629ad11d96eb2444be14ff
dangphu2412 <dangphu241299@gmail.com>
2023-10-31 13:57:13 +0700

    feat: add params validation

--- packages/core/src/factories/module.factory.ts
+++ packages/core/src/factories/module.factory.ts
@@ -6,6 +6,10 @@ type ModuleFactory = (app: Router) => void;
 export function createModuleFactory({ path, bundler }: ModuleProperty): ModuleFactory {
     const router = Router();
 
+    if (!path || typeof path !== 'string') {
+        throw new TypeError('Invalid path when calling createModuleFactory. Expected to be string');
+    }
+
     return (app: Router) => {
         bundler(router);
 
