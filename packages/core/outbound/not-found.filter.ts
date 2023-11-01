eea53f4796a1cf3ccba9961733a65c69e8e07680
dangphu2412 <dangphu241299@gmail.com>
2023-11-01 10:16:50 +0700

    chore: bump version

--- packages/core/outbound/not-found.filter.ts
+++ packages/core/outbound/not-found.filter.ts
@@ -1,7 +1,8 @@
 import type { Handler, Request, Response } from 'express';
+import { NOT_FOUND } from 'http-status';
 
 export const notFoundHandler: Handler = (req: Request, res: Response) => {
-    return res.status(404).json({
+    return res.status(NOT_FOUND).json({
         message: `Can not ${req.method} ${req.path}`,
     });
 }
